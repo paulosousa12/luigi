c90fee137cd45ac051a56c46c4784bd9067b46a4
Paulo Sousa <paulo.sousa@xing.pt>
2020-01-31 15:13:07 +0000

    Improving crontab

--- worker/tasks/crontask.py
+++ worker/tasks/crontask.py
@@ -1,12 +1,16 @@
 import luigi
-import datetime
+from worker.tasks.example import RunExampleTask
 from luigi.local_target import LocalTarget
 
 
 class CronTask(luigi.Task):
+    def requires(self):
+        for i in range(10):
+            yield RunExampleTask(i)
+
     def run(self):
         with self.output().open('w') as f:
-            f.write("{}\n".format(datetime.datetime.now()))
+            f.write('All done!')
 
     def output(self):
         return LocalTarget('tmp/RunAllTasks.txt')
